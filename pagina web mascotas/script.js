d81f833a2595377ef4146525fc57cb6372cd168c
Zakrosus <109133932+Zakrosus@users.noreply.github.com>
2023-03-31 23:21:06 -0500

    Add files via upload

---
+++ pagina web mascotas/script.js
@@ -0,0 +1,38 @@
+// Validación de formulario de contacto
+function validateForm() {
+    var name = document.forms["contactForm"]["name"].value;
+    var email = document.forms["contactForm"]["email"].value;
+    var message = document.forms["contactForm"]["message"].value;
+    if (name == "" || email == "" || message == "") {
+      alert("Por favor, complete todos los campos del formulario.");
+      return false;
+    }
+  }
+  
+  // Carrusel de imágenes de mascotas
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  }
